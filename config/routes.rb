93f190907b015dc1bdad7f368f2255eb9e6fcab1
pierre <pierre@fx-pierre.(none)>
2010-03-30 13:05:16 +0200

    bug fix products controller & missing translation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # Put your extension routes here.
 
 map.namespace :admin do |admin|
-  admin.resources :product do |product|
+  admin.resources :products do |product|
     product.resources :movies
   end
   admin.resources :movie_sites
